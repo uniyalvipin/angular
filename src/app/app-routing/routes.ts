88c2c724fb6aac84cfb98418cbbbcd447a7ebeab
vipin uniyal <uniyalvipin70@gmail.com>
2022-08-31 01:27:42 +0530

    Integration of font awesome Added header and footer Angular router basics

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,16 @@
+import { Route } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishDetailComponent } from '../dish-detail/dish-detail.component';
+import { AboutComponent } from '../about/about.component';
+import { HomeComponent } from '../home/home.component';
+import { ContactComponent } from '../contact/contact.component';
+
+export const routes: Route[] = [
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  // { path: 'dishdetail/:id', component: DishDetailComponent },
+  // { path: 'contactus', component: ContactComponent },
+  // { path: 'aboutus', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-import { MenuComponent } from './menu/menu.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 
+
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishDetailComponent],
+  declarations: [AppComponent, MenuComponent, DishDetailComponent, HeaderComponent, FooterComponent, AboutComponent, HomeComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
