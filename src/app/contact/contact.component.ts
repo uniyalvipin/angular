a5135ed7a90d2dfe3130a758a360190efa1ca246
vipin uniyal <uniyalvipin70@gmail.com>
2022-09-02 01:18:24 +0530

    Reactive Forms Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 
@@ -8,6 +8,8 @@ import { Feedback, ContactType } from '../shared/feedback';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  @ViewChild('fform') feedbackFormDirective: any;
+
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
@@ -20,12 +22,12 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: null,
-      email: '',
+      firstname: ['', [Validators.required, Validators.minLength(2)]],
+      lastname: ['', [Validators.required, Validators.minLength(2)]],
+      telnum: [0, [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
-      contacttype: 'None',
+      contactType: 'None',
       message: '',
     });
   }
@@ -33,6 +35,15 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contactType: 'None',
+      message: '',
+    });
+    this.feedbackFormDirective.resetForm();
   }
 }
