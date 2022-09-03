e62b7c28788081f36a3bf7e3ee3c8b9360e8d568
vipin uniyal <uniyalvipin70@gmail.com>
2022-09-03 20:19:06 +0530

    Assignment 3 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
@@ -64,6 +65,7 @@ import { LeaderService } from './services/leader.service';
     MatSlideToggleModule,
     ReactiveFormsModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
   ],
   entryComponents: [LoginComponent],
   providers: [DishService, PromotionService, LeaderService],
--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Dish } from '../shared/dish';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Dish, Comment } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dish-detail',
@@ -15,12 +16,37 @@ export class DishDetailComponent implements OnInit {
   dishIds: string[];
   prev: string;
   next: string;
+  commentForm: FormGroup;
+  comment: Comment;
+  showPreview: boolean;
+
+  @ViewChild('cform') commentFormDirective: any;
 
   constructor(
     private dishService: DishService,
     private route: ActivatedRoute,
-    private location: Location
-  ) {}
+    private location: Location,
+    private fb: FormBuilder
+  ) {
+    this.createCommentForm();
+  }
+
+  formatLabel = (value: number) => value;
+
+  formErrors: any = {
+    comment: '',
+    author: '',
+  };
+
+  validationMessages: any = {
+    comment: {
+      required: 'Comment is required',
+    },
+    author: {
+      required: 'Author is required',
+      minlength: 'Last Name must be at least 2 characters long.',
+    },
+  };
 
   ngOnInit(): void {
     this.dishService
@@ -34,6 +60,64 @@ export class DishDetailComponent implements OnInit {
         this.dish = dish;
         this.setPrevNext(dish.id);
       });
+  }
+
+  createCommentForm() {
+    console.log('created form');
+
+    this.commentForm = this.fb.group({
+      comment: ['', [Validators.required]],
+      rating: [5],
+      author: ['', [Validators.required, Validators.minLength(2)]],
+      date: [new Date().toISOString()],
+    });
+
+    this.commentForm.valueChanges.subscribe((data) =>
+      this.onValueChanged(data)
+    );
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) return;
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+
+    if (form.valid) {
+      this.showPreview = true;
+    } else {
+      this.showPreview = false;
+    }
+  }
+
+  submitComment() {
+    this.comment = this.commentForm.value;
+    this.dish?.comments?.push(this.comment);
+
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: '',
+      date: [new Date().toISOString()],
+    });
+    this.commentFormDirective.resetForm({
+      author: '',
+      rating: 5,
+      comment: '',
+      date: [new Date().toISOString()],
+    });
   }
 
   setPrevNext(dishId: string) {
